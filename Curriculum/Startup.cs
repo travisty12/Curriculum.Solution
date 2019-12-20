bba4030386152c070991669fd02d622c96571863
Travis Toal <travisty12@gmail.com>
2019-12-20 15:50:06 -0800

    add password config

--- Curriculum/Startup.cs
+++ Curriculum/Startup.cs
@@ -33,7 +33,15 @@ namespace Curriculum
         .AddEntityFrameworkStores<CurriculumContext>()
         .AddDefaultTokenProviders();
       
-      
+      services.Configure<IdentityOptions>(options => 
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     
